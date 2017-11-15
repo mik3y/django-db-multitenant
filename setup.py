be49d1b73bff11ada412d646eca62ebd37c64efe
Stéphane Raimbault <stephane.raimbault@gmail.com>
2017-11-15 16:10:32 +0100

    Migrate pseudo Markdown README.md to README.rst

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ REQUIRED = [
 here = os.path.abspath(os.path.dirname(__file__))
 
 # Import the README and use it as the long-description.
-with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = '\n' + f.read()
 
 # Load the package's __version__.py module as a dictionary.
